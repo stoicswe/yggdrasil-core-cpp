b27861659242acb19ec7473001f5e95d972dcf4d
Nathaniel <31823043+stoicswe@users.noreply.github.com>
2022-11-07 17:37:25 -0500

    Fixed the deps linking

--- BuildLinux_Old.sh
+++ BuildLinux_Old.sh
@@ -6,8 +6,7 @@ then
   then
     rm -rf ./cmake-build-debug
     mkdir ./cmake-build-debug
-    conan install . -s build_type=Debug --install-folder=cmake-build-debug
-    #cd ./cmake-build-debug
+    cd ./cmake-build-debug
   else
     echo "Debug build clean."
   fi
@@ -16,15 +15,14 @@ then
   then
     rm -rf ./cmake-build-release
     mkdir ./cmake-build-release
-    conan install . -s build_type=Release --install-folder=cmake-build-release
     cd ./cmake-build-release
   else
     echo "Release build clean."
   fi
 fi
-#conan profile new default --detect
-#conan profile update settings.compiler.libcxx=libstdc++11 default
-#conan install .. --build=missing
+conan profile new default --detect
+conan profile update settings.compiler.libcxx=libstdc++11 default
+conan install .. --build=missing
 if [ $buildMake == true ]
 then
   cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
