e0865496a7dea0b060102f5100cef82d9a812187
Nathaniel Knudsen <31823043+stoicswe@users.noreply.github.com>
2022-11-21 16:27:44 -0500

    First sample controller

--- src/api/api.h
+++ src/api/api.h
@@ -1,7 +1,8 @@
 #include "oatpp/web/server/api/ApiController.hpp"
 #include "oatpp/core/macro/codegen.hpp"
+#include "oatpp/core/macro/component.hpp"
 
-#include OATPP_CODEGEN_BEGIN(ApiController)
+
 
 namespace api
 {
@@ -12,18 +13,28 @@ namespace api
 
     };
 
+#include OATPP_CODEGEN_BEGIN(ApiController)
+
     class BlockchainController : public oatpp::web::server::api::ApiController
     {
         public:
-            BlockchainController() = default;
-            virtual ~BlockchainController() = default;
-
-            BlockchainController(OATPP_COMPONENT(std::shared_ptr<ObjectMapper> objectMapper))
+            BlockchainController(const std::shared_ptr<ObjectMapper>& objectMapper)
                 : oatpp::web::server::api::ApiController(objectMapper)
             {}
 
+            static std::shared_ptr<BlockchainController> createShared(OATPP_COMPONENT(std::shared_ptr<ObjectMapper>, objectMapper)) {
+                return std::shared_ptr<BlockchainController>(new BlockchainController(objectMapper));
+            }
+
             std::shared_ptr<BlockchainService> m_blockChainService;
+
+            ENDPOINT("GET", "/", root) {
+                return createResponse(Status::CODE_200, "API IS LIVE");
+            }
+
+            // TODO: More endpoints
     };
-}
 
 #include OATPP_CODEGEN_END(ApiController)
+}
+
